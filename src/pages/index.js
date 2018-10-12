db5027c51233b432c290fb5fa1ba35e33152a01f
Thomas Ingram <code@ravinggenius.com>
2018-10-11 22:45:09 -0400

    Rip out unused page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import { Link } from 'gatsby';
 import React from 'react';
 
 import Layout from '../components/layout';
@@ -8,7 +7,6 @@ const IndexPage = () => (
 		<h1>Hi people</h1>
 		<p>Welcome to your new Gatsby site.</p>
 		<p>Now go build something great.</p>
-		<Link to="/page-2/">Go to page 2</Link>
 	</Layout>
 );
 
