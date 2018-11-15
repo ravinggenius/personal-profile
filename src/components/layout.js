a784c545f9ad2233a1121d891a90b84767becc51
Thomas Ingram <code@ravinggenius.com>
2018-11-14 20:37:38 -0500

    General code cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,24 @@
 import { StaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled, { injectGlobal } from 'react-emotion';
+import styled, { css } from 'react-emotion';
 import Helmet from 'react-helmet';
 
 import 'normalize.css';
 
-injectGlobal`
-	body {
-		--angle: calc(90deg + 75deg);
-		--primary: darkslateblue;
-		--secondary: aquamarine;
-		background-attachment: fixed;
-		background-color: var(--primary);
-		background-image: linear-gradient(
-			var(--angle),
-			var(--primary) 0,
-			var(--primary) 30%,
-			var(--secondary) 100%
-		);
-		min-height: 100vh;
-	}
+const bodyStyles = css`
+	--angle: calc(90deg + 75deg);
+	--primary: darkslateblue;
+	--secondary: aquamarine;
+	background-attachment: fixed;
+	background-color: var(--primary);
+	background-image: linear-gradient(
+		var(--angle),
+		var(--primary) 0,
+		var(--primary) 30%,
+		var(--secondary) 100%
+	);
+	min-height: 100vh;
 `;
 
 const Main = styled.main`
@@ -43,14 +41,15 @@ const Layout = ({ children }) => (
 		`}
 		render={data => (
 			<Main>
-				<Helmet
-					title={data.site.siteMetadata.title}
-					meta={[
-						{ name: 'description', content: 'Sample' },
-						{ name: 'keywords', content: 'sample, something' }
-					]}
-				>
+				<Helmet>
 					<html lang="en" />
+
+					<title>{data.site.siteMetadata.title}</title>
+
+					<meta name="description" contents="Sample" />
+					<meta name="keywords" contents="sample, something" />
+
+					<body className={bodyStyles} />
 				</Helmet>
 
 				{children}
