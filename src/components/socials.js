43730e99e90dbd4df07d6f9db4450ea991c37705
Thomas Ingram <code@ravinggenius.com>
2018-11-21 20:36:03 -0500

    List social medias horizontally

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import styled from 'react-emotion';
 
 import Section from './section';
 import Email from './socials/email';
 import Phone from './socials/phone';
 import ThirdParty from './socials/third_party';
 
+const Wrapper = styled(Section)`
+	display: flex;
+	justify-content: space-evenly;
+	flex-wrap: wrap;
+`;
+
 const Socials = ({ email, phone, socials }) => {
 	if (email || phone || socials.length) {
 		return (
-			<Section>
+			<Wrapper>
 				<Phone display={phone} />
 				<Email {...{ email }} />
 				{socials.map(({ code, handle }) => (
@@ -17,7 +24,7 @@ const Socials = ({ email, phone, socials }) => {
 						key={`${code}-${handle}`}
 					/>
 				))}
-			</Section>
+			</Wrapper>
 		);
 	}
 };
